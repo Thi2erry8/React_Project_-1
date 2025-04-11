8ac2ddbc841cf37fcda13486297d8b24ad02adee
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-04-10 22:28:29 -0400

    4

--- 3/Nasa(API)/src/assets/SideBar.jsx
+++ 3/Nasa(API)/src/assets/SideBar.jsx
@@ -1,4 +1,12 @@
 function SideBar(){
-
+    return(
+         <div>
+               <h2>The Brutal Martian Landscape</h2>
+               <div>
+                     <p>description</p>
+                     
+               </div>
+         </div>
+    )
 }
 export default SideBar
